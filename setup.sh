3e81334e83ecba11c7480eca75f6e4ed8093c2de
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2024-07-05 22:30:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,17 +14,30 @@ banner()
 
 termux_app()
 {
-
+  cp -r proxyace /data/data/com.termux/files/usr/bin/
+  chmod +x /data/data/com.termux/files/usr/bin/proxyace
+  if [[ $(command -v proxyace) ]]; then
+    echo ''
+  else
+    echo ''
+  fi
 }
 
 linux_setup()
 {
-
+  cp -r proxyace /usr/bin/
+  chmod +x /usr/bin/proxyace
+  if [[ $(command -v proxyace) ]]; then
+    echo ''
+  else
+    echo ''
+  fi
 }
 
 main()
 {
   banner
+  sleep 3
   if [[ -d /usr/bin ]]; then
     linux_setup
   else
